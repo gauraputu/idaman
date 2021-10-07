d780f73c3bcee19b8e1499f351eefcafba3ce20d
gaura <email@example.com>
2021-10-07 16:34:33 +0700

    push from another machine

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+const Layout = (props) => (
+        <Head>
+            <title>iDaman</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+);
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import styles2 from '../styles/textBox.module.css'
+import Script from 'next/script'
+import Layout from '../components/Layout'
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+() => import('/public/static/query.js'),
+{ ssr: false }
+)
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>iDaman</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <Layout />
       <main className={styles.main}>
         <h1 className={styles.title}>
           PSB Alter Create
         </h1>
         <textarea className={styles2.textBox} placeholder="paste telegram text here!"></textarea>
-        <button>Process</button>
+        <button className={styles2.btnNormal} id="process">Process</button>
+        {/* <Script src="/public/static/query.js" /> */}
+        <DynamicComponentWithNoSSR />
       </main>
 
       <footer className={styles.footer}>

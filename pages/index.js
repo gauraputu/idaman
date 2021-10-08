419664eef24d7f1d0fb4465a0de56a81b0e64ae5
gaura <email@example.com>
2021-10-08 15:38:39 +0700

    fix importing external javascript

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import styles2 from '../styles/textBox.module.css'
-import Script from 'next/script'
 import Layout from '../components/Layout'
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(
-() => import('/public/static/query.js'),
-{ ssr: false }
-)
+import {proses, sheetData} from '../public/static/query.js'
 
 export default function Home() {
   return (
@@ -19,14 +11,13 @@ export default function Home() {
         <h1 className={styles.title}>
           PSB Alter Create
         </h1>
-        <textarea className={styles2.textBox} placeholder="paste telegram text here!"></textarea>
-        <button className={styles2.btnNormal} id="process">Process</button>
-        {/* <Script src="/public/static/query.js" /> */}
-        <DynamicComponentWithNoSSR />
+        <textarea className={styles2.textBox} id="telegramText" placeholder="paste telegram text here!"></textarea>
+        <button className={styles2.btnNormal} onClick={(e)=>{proses()}}>Process</button>
+        {/* {sheetData()} */}
       </main>
 
       <footer className={styles.footer}>
-          Made With ♥{' '} Version 0.30
+          Made With ♥ Version 0.30
       </footer>
     </div>
   )
--- public/static/query.js
+++ public/static/query.js
@@ -1,5 +1,28 @@
-console.log("script loaded!")
-function hello(){
-    console.log("hello!")
-}
-// document.getElementById("process").addEventListener("click", hello()); 
+//download data from google sheet in the form of array
+export function sheetData(){
+    
+    async function get() {
+        let url = 'https://script.google.com/macros/s/AKfycbxh6ENRLKyYif-w5j-pder-nGVIN-QL_Y88O5DOBCDxIEuHrhQS_1XR0S1om6uPcMwy3Q/exec'
+        let obj = await (await fetch(url)).json();
+        
+        return obj;
+    }
+
+    let data;
+    (async () => {
+      data = await get()
+    //   console.log(data)
+    })()
+    // console.log("function run")
+    return {data}
+}  
+
+//process input
+export function proses(){
+    let text = document.getElementById("telegramText").value;
+    let ip = text.match(/\d+\.\d+\.\d+\.\d+/gi);
+    let service = text.match(/\d+_\d+_\w+/gi);
+    let slot = text.match(/(?=.*slot)\D*(\d+)|(\d+)\/(\d+)\/(\d+)/i); //['slot',pattern1,pattern2,pattern2]
+    let port = text.match(/(?=.*port)\D*(\d+)|(\d+)\/(\d+)\/(\d+)/i); //['port',pattern1,pattern2,pattern2]
+    
+  };

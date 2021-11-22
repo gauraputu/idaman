af6e0f6aab43d52a4afb3e2fda6580da2663fcde
gaura <email@example.com>
2021-11-22 16:15:32 +0700

    working prototype content in markdown

--- pages/sop2.js
+++ pages/sop2.js
@@ -1,15 +1,22 @@
 import Navbar from '../components/Navbar'
 const showdown  = require('showdown');
-
-const sop2 = () =>{
- let   converter = new showdown.Converter(),
-    text      = '# hello, markdown!',
-    html      = converter.makeHtml(text);
+const fs  = require("fs");
+// const postDirectory = join(process.cwd(),'_posts')
+const sop2 = (props) =>{
     return (
         <>
         <Navbar />
-        <div dangerouslySetInnerHTML={{ __html:html}}/> 
+        <div dangerouslySetInnerHTML={{ __html:props.body}}/> 
         </>
     )
+}
+export async function getStaticProps(){
+    let content = fs.readFileSync('./_posts/sop.md','utf8');
+    let converter = new showdown.Converter();
+    let html = converter.makeHtml(content);
+
+    return {
+       props : {text:'#  this is markdown',body:html}
+    }
 }
 export default sop2
